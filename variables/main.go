796c00ed315dc18ae8cdd2da203011617ad853d5
Ben <benjamin.slabbert@gmail.com>
2019-01-03 16:13:31 +0200

    declaring variables

--- variables/main.go
+++ variables/main.go
@@ -4,14 +4,31 @@ import (
 	"fmt"
 )
 
+var GLOBAL = 10
+
 func main() {
 
+	// short declaration
+	// declare and assign value
 	x := 42
 	fmt.Println(x)
 
-	//print address of the variable (pointers :D)
+	// print address of the variable (pointers :D)
 	fmt.Println(&x)
 
 	i := &x
 	fmt.Println(*i)
+
+	declaring()
+}
+
+func declaring() {
+	var y int16 = 5
+	fmt.Println(y)
+	useType(y)
+}
+
+func useType(p ...int16) {
+	fmt.Println(p[0])
+	fmt.Println(GLOBAL)
 }
