a43100b1dcfd6c128bdb7b479ec64eec19726d65
Ben <benjamin.slabbert@gmail.com>
2019-01-24 14:54:11 +0200

    adding structs

---
+++ struct/struct.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+	last  string
+}
+
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+func main() {
+
+	p1 := person{
+		first: "james",
+		last:  "bond",
+	}
+
+	fmt.Println(p1)
+	fmt.Println(p1.first)
+	fmt.Println(p1.last)
+
+	agent := secretAgent{
+		person: p1,
+		ltk:    true,
+	}
+
+	fmt.Println(agent)
+	fmt.Println(agent.person)
+	fmt.Println(agent.ltk)
+}
