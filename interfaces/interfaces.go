7a23654bb868eec33718a2a9112a3193ecc3dab9
Ben <benjamin.slabbert@gmail.com>
2019-01-24 15:59:39 +0200

    adding interface

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -1,5 +1,40 @@
 package main
 
+import "fmt"
+
+type person struct {
+	first string
+	last  string
+}
+
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+func (s secretAgent) speak() {
+	fmt.Println("my name is", s.first, ",", s.last)
+}
+
+type human interface {
+	speak()
+}
+
 func main() {
 
+	sa1 := secretAgent{
+		person: person{
+			first: "james",
+			last:  "bond",
+		},
+		ltk: true,
+	}
+
+	fmt.Println(sa1)
+
+	useHuman(sa1)
+}
+
+func useHuman(h human) {
+	h.speak()
 }
